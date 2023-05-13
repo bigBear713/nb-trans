01fca1c25453b17326070d0eb0ecbfd371fa9e27
bigBear713 <774326739@qq.com>
2023-05-13 21:51:12 +0800

    refactor: update handleSentenceWithParams function

--- projects/nb-trans/src/lib/services/nb-trans-tools.service.ts
+++ projects/nb-trans/src/lib/services/nb-trans-tools.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { INbTransSentencePart, INbTransParams } from '../models';
 import { NbValueTypeService } from '@bigbear713/nb-common';
+import { nbParamKeyRegExp2Split, paramKeyRegExpRule } from '../constants/nb-param-key-regexp';
 
 @Injectable({ providedIn: 'root' })
 export class NbTransToolsService {
@@ -19,10 +20,6 @@ export class NbTransToolsService {
     return prefix ? `${prefix}.${key}` : key;
   }
 
-  handleSentence(str: string, searchStr: string, replaceStr: string): string {
-    return str.replace(new RegExp(searchStr, 'g'), replaceStr);
-  };
-
   handleSentenceWithParams(trans: string, params?: INbTransParams): string {
     if (!params) {
       return trans;
@@ -34,14 +31,14 @@ export class NbTransToolsService {
     }
 
     // First, split the trans string to string array via params key,
-    // like this: 'This is {{p1}} and {{p2}} and {{p1}}.' -> 
-    // ['This is ','{{p1}}',' and ','{{p2}}',' and ','{{p1}}','.'] 
+    // like this: 'This is {{p1}} and {{p2}} and {{p1}}.' --> 
+    // ['This is ','{{p1}}',' and ','{{p2}}',' and ','{{p1}}','.'].
     // Then replace the params key as params value, like this:
     // ['This is ','param1',' and ','param2',' and ','param1','.'] 
     // Last, make array join as string, like this: 'This is param1 and param2 and param1.'
-    const { specialKeys, specialParams } = this.converParams2SpecialKey(params, paramsKeys)
-    const splitStrArr = this.splitTrans(trans, specialKeys);
-    return this.replaceAsParamsValueInSplitArr(splitStrArr, specialParams).join('');
+    const cleanedParams = this.cleanParams(params, paramsKeys);
+    const splitStrArr = trans.split(nbParamKeyRegExp2Split);
+    return this.replaceAsParamsValueInSplitArr(splitStrArr, cleanedParams).join('');
   }
 
   handleTrans(trans: string): INbTransSentencePart[] {
@@ -69,12 +66,21 @@ export class NbTransToolsService {
     return sentenceList;
   }
 
-  private handleCompStr(content: string): {
-    index: number;
-    content: string;
-    list: INbTransSentencePart[];
-    otherContent: string;
-  } | string {
+  private cleanParams(params: INbTransParams, paramsKeys: string[]) {
+    // because after calling RegExp's test function, the lastIndex value will be changed, so have to set it as 0.
+    // so create a new value every time to make sure the regexp will not affect anywhere
+    const nbParamKeyRegExp = new RegExp(paramKeyRegExpRule, 'g');
+    return paramsKeys.filter(key => {
+      const isValid = nbParamKeyRegExp.test(key);
+      nbParamKeyRegExp.lastIndex = 0;
+      return isValid;
+    }).reduce((prev, key) => {
+      prev[`{{${key}}}`] = params[key];
+      return prev;
+    }, {} as INbTransParams);
+  }
+
+  private handleCompStr(content: string) {
     const startFlagIndex = content.search(/<\d+>/);
     if (startFlagIndex === -1) {
       return content;
@@ -102,23 +108,7 @@ export class NbTransToolsService {
     };
   }
 
-  private converParams2SpecialKey(params: INbTransParams, paramsKeys: string[]) {
-    const defaultValue: {
-      specialKeys: string[],
-      specialParams: INbTransParams,
-    } = {
-      specialKeys: [],
-      specialParams: {}
-    };
-    return paramsKeys.reduce((prev, key) => {
-      const specialKey = `{{${key}}}`;
-      prev.specialKeys.push(specialKey);
-      prev.specialParams[specialKey] = params[key];
-      return prev;
-    }, defaultValue);
-  }
-
-  private replaceAsParamsValueInSplitArr(transSplitArr: string[], params: INbTransParams): string[] {
+  private replaceAsParamsValueInSplitArr(transSplitArr: string[], params: INbTransParams) {
     transSplitArr.forEach((item, index) => {
       const paramValue = params[item];
       if (paramValue) {
@@ -128,11 +118,4 @@ export class NbTransToolsService {
     return transSplitArr;
   }
 
-  private splitTrans(trans: string, specialKeys: string[]): string[] {
-    // splitter like this: /({{p1}}|{{p2}})/g
-    const splitter = new RegExp(`(${specialKeys.join('|')})`, 'g');
-    // origin string: 'This is {{p1}} and {{p2}} and {{p1}}.'
-    // expected result: ['This is ','{{p1}}',' and ','{{p2}}',' and ','{{p1}}','.']
-    return trans.split(splitter);
-  }
 }
